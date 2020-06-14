8556354bcba3bdc40fbc4710f07d68b40d84af50
jaam92 <jazlyn.mooney@gmail.com>
2020-06-14 12:09:51 -0600

    redid all the plots

--- LocalRscripts/annotateVCF/PlotAnnotations.R
+++ LocalRscripts/annotateVCF/PlotAnnotations.R
@@ -1,79 +1,97 @@
 #Load Libraries
-source("~/Documents/DogProject_Jaz/OMIA/R_rainclouds.R")
+source("~/Documents/DogProject_Jaz/LocalRscripts/OMIA/R_rainclouds.R")
 library(ggplot2)
-library(cowplot)
+library(ggpubr)
 library(dplyr)
 library(mgsub)
 
+#Plotting fxn and color palette
+cbPalette = c("Arctic Wolf" = "gray25", "Ethiopian Wolf" = "#D55E00",  "Isle Royale" = "steelblue", "Border Collie" = "#009E73", "Labrador Retriever" = "gold3", "Pug" = "mediumpurple4", "Tibetan Mastiff" = "#CC79A7")
+
+plotFxn = function(colOfInterest, axisTitle) {
+  RaincloudWithBoxPlot = ggplot(PlotDF, aes(x=Population, y=colOfInterest, colour=Population)) +
+    geom_flat_violin(size=1, position = position_nudge(x = .25, y = 0),adjust =2, trim = FALSE) +
+    geom_point(aes(x = as.numeric(Population)-.15, y = colOfInterest, colour = Population),position = position_jitter(width = .05), size = 1, shape = 20) +
+    geom_boxplot(aes(as.numeric(Population), y = colOfInterest, fill = Population), outlier.shape = NA, alpha = .5, width = .1, colour = "black") +
+    coord_flip() +
+    guides(fill = FALSE, colour = FALSE) +
+    scale_colour_manual(values = cbPalette) +
+    scale_fill_manual(values = cbPalette) + 
+    labs(x="Population",y=paste0(axisTitle)) + 
+    theme_bw() + 
+    theme(axis.text.x = element_text(size=20, angle = 45, vjust=0.7), 
+          axis.text.y = element_text(size=20), 
+          plot.title=element_text(size=24, hjust = 0.5, face = "bold"), 
+          axis.title=element_text(size=24),
+          legend.title=element_text(size=20), 
+          legend.text=element_text(size=20))
+  return(RaincloudWithBoxPlot)
+}
+
 #Read file in 
-df = read.delim("~/Documents/DogProject_Clare/LocalRscripts/annotateVCF/AllChroms/GTAnnotationCountResults_Dec2018_DogProjClare.txt")
+df = read.delim("~/Documents/DogProject_Clare/LocalRscripts/annotateVCF/AllChroms/GTAnnotationCountResults_May2019_DogProjClare.txt", stringsAsFactors = F)
 
 #Plot Counts SYN Derived Alleles
 PlotDF = df %>% 
-  mutate(CallableSites = LineCount - Missing, 
-         PropSY_CountAlleles = SY_CountAlleles/CallableSites,
-         PropNS_CountAlleles = NS_CountAlleles/CallableSites,
-         PropLOF_CountAlleles = LOF_CountAlleles/CallableSites)
+  mutate(CallableSites = LineCount - Missing)
 
-scaleCalls = mean(PlotDF$CallableSites)
-
-
-PlotDF$Population = mgsub_dict(PlotDF$Population,conversions =  list("BC" = "Border Collie", "LB" = "Labrador Retriever", "PG" = "Pug", "TM" = "Tibetan Mastiff", "AW" = "Arctic Wolf", "EW" = "Ethiopian Wolf", "IR" = "Isle Royale"))
+PlotDF$Population = mgsub(PlotDF$Population, 
+                          pattern =c("BC", "LB", "PG", "TM", "AW", "EW", "IR"), replacement =c("Border Collie", "Labrador Retriever", "Pug", "Tibetan Mastiff", "Arctic Wolf",  "Ethiopian Wolf", "Isle Royale"))
 orderPops = c("Border Collie", "Labrador Retriever", "Pug", "Tibetan Mastiff", "Arctic Wolf",  "Ethiopian Wolf", "Isle Royale")
 PlotDF$Population = factor(PlotDF$Population, levels = orderPops)
 
-cbPalette = c("Arctic Wolf" = "gray25", "Ethiopian Wolf" = "#D55E00",  "Isle Royale" = "steelblue", "Border Collie" = "#009E73", "Labrador Retriever" = "gold3", "Pug" = "mediumpurple4", "Tibetan Mastiff" = "#CC79A7")
+###Plot Supplementary Figures putativeley neutral and deleterious
+CountDerHom_PutDel = plotFxn(PlotDF$PutDel_CountDerHom, "Count Derived Homozygotes") 
+CountDerHom_PutNeu = plotFxn(PlotDF$PutNeu_CountDerHom, "Count Derived Homozygotes")
 
-(SYAlleles = ggplot(PlotDF, aes(x=Population, y=PropSY_CountAlleles*scaleCalls,colour=Population, label=ID)) + geom_boxplot()+ scale_colour_manual(name="Group", values = cbPalette) + labs(x="Group",y="Synonymous Derived Alleles") + theme_bw() + theme(axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20), plot.title=element_text(size=26, face = "bold", hjust=0.5), axis.title=element_text(size=20)) + theme(legend.title=element_text(size=20), legend.text=element_text(size=18)))
+CountVar_PutDel = plotFxn(PlotDF$PutDel_CountVariants, "Count Variants") 
+CountVar_PutNeu = plotFxn(PlotDF$PutNeu_CountVariants, "Count Variants")
 
+CountAllele_PutDel = plotFxn(PlotDF$PutDel_CountAlleles, "Count Alleles") 
+CountAllele_PutNeu = plotFxn(PlotDF$PutNeu_CountAlleles, "Count Alleles")
 
-(NSAlleles = ggplot(PlotDF, aes(x=Population, y=PropNS_CountAlleles*scaleCalls,colour=Population, label=ID)) + geom_boxplot()+ scale_colour_manual(name="Group", values = cbPalette) + labs(x="Group",y="Nonsynonymous Derived Alleles") + theme_bw() + theme(axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20), plot.title=element_text(size=26, face = "bold", hjust=0.5), axis.title=element_text(size=20)) + theme(legend.title=element_text(size=20), legend.text=element_text(size=18)))
+#Neutral
+putNeu = ggarrange( CountDerHom_PutNeu + theme(axis.title.y=element_blank(), axis.title.x=element_blank()) + ggtitle("Count Homozygotes"),
+                    CountVar_PutNeu + theme(axis.title.y=element_blank(), axis.title.x=element_blank()) + ggtitle("Count Variants"),
+                    CountAllele_PutNeu + theme(axis.title.y=element_blank(), axis.title.x=element_blank()) + ggtitle("Count Alleles"),
+                    align = 'hv',
+                    labels = c("A", "B", "C"),
+                    hjust = -1,
+                    nrow = 1)
+putNeuAnnot = annotate_figure(putNeu, left = text_grob("Neutral", size=24, face="bold",rot = 90, hjust = 0.5))
 
+#Deleterious
+putDel = ggarrange( CountDerHom_PutDel + theme(axis.title.y=element_blank(), axis.title.x=element_blank()),
+                    CountVar_PutDel + theme(axis.title.y=element_blank(), axis.title.x=element_blank()),
+                    CountAllele_PutDel + theme(axis.title.y=element_blank(), axis.title.x=element_blank()),
+                    align = 'hv',
+                    labels = c("D", "E", "F"),
+                    hjust = -1,
+                    nrow = 1)
+putDelAnnot = annotate_figure(putDel, left = text_grob("Deleterious", size=24, face="bold",rot = 90, hjust = 0.5))
 
-(LOFAlleles = ggplot(PlotDF, aes(x=Population, y=PropLOF_CountAlleles*scaleCalls,colour=Population, label=ID)) + geom_boxplot()+ scale_colour_manual(name="Group", values = cbPalette) + labs(x="Group",y="LOF Derived Alleles") + theme_bw() + theme(axis.text.x = element_text(size = 20), axis.text.y = element_text(size = 20), plot.title=element_text(size=26, face = "bold", hjust=0.5), axis.title=element_text(size=20)) + theme(legend.title=element_text(size=20), legend.text=element_text(size=18)))
-
-
-
-pSY =ggplot(PlotDF,aes(x=Population, y=PropSY_CountAlleles*scaleCalls,colour=Population)) +
-  geom_flat_violin(size=1, position = position_nudge(x = .25, y = 0),adjust =2, trim = FALSE) +
-  geom_point(aes(x = as.numeric(Population)-.15, y = PropSY_CountAlleles*scaleCalls, colour = Population),position = position_jitter(width = .05), size = 1, shape = 20) +
-  geom_boxplot(aes(as.numeric(Population), y = PropSY_CountAlleles*scaleCalls, fill = Population),outlier.shape = NA, alpha = .5, width = .1, colour = "black") +
-  coord_flip() +
-  guides(fill = FALSE, colour = FALSE) +
-  scale_colour_manual(values = cbPalette) +
-  scale_fill_manual(values = cbPalette) + 
-  labs(x="Group",y="Synonymous Derived Alleles") + 
-  theme(plot.title=element_text(size =18, face = "bold", hjust=0.5), axis.text.x = element_text(size  = 24, vjust=1, hjust=0.5), axis.text.y = element_text(size  = 24), axis.title=element_text(size=24))
-
-
-pNS = ggplot(PlotDF,aes(x=Population, y=PropNS_CountAlleles*scaleCalls,colour=Population)) +
-  geom_flat_violin(size=1, position = position_nudge(x = .25, y = 0),adjust =2, trim = FALSE) +
-  geom_point(aes(x = as.numeric(Population)-.15, y = PropNS_CountAlleles*scaleCalls, colour = Population),position = position_jitter(width = .05), size = 1, shape = 20) +
-  geom_boxplot(aes(as.numeric(Population), y = PropNS_CountAlleles*scaleCalls, fill = Population),outlier.shape = NA, alpha = .5, width = .1, colour = "black") +
-  coord_flip() +
-  guides(fill = FALSE, colour = FALSE) +
-  scale_colour_manual(values = cbPalette) +
-  scale_fill_manual(values = cbPalette) + 
-  labs(x="Group",y="Nonsynonymous Derived Alleles") +
-  theme(plot.title=element_text(size =18, face = "bold", hjust=0.5), axis.text.x = element_text(size  = 24, vjust=1, hjust=0.5), axis.text.y = element_text(size  = 24), axis.title=element_text(size=24))
-
-pLOF =ggplot(PlotDF,aes(x=Population, y=PropLOF_CountAlleles*scaleCalls,colour=Population)) +
-  geom_flat_violin(size=1, position = position_nudge(x = .25, y = 0),adjust =2, trim = FALSE) +
-  geom_point(aes(x = as.numeric(Population)-.15, y = PropLOF_CountAlleles*scaleCalls, colour = Population),position = position_jitter(width = .05), size = 1, shape = 20) +
-  geom_boxplot(aes(as.numeric(Population), y = PropLOF_CountAlleles*scaleCalls, fill = Population),outlier.shape = NA, alpha = .5, width = .1, colour = "black") +
-  coord_flip() +
-  guides(fill = FALSE, colour = FALSE) +
-  scale_colour_manual(values = cbPalette) +
-  scale_fill_manual(values = cbPalette) + 
-  labs(x="Group",y="Loss of Function Derived Alleles") +
-  theme(plot.title=element_text(size =18, face = "bold", hjust=0.5), axis.text.x = element_text(size  = 24, vjust=1, hjust=0.5), axis.text.y = element_text(size  = 24), axis.title=element_text(size=24))
+#plot deleterious and neutral
+ggarrange(putNeuAnnot, putDelAnnot, nrow = 2)
 
 
-# arrange the three plots in a single row
-plot_grid( pSY,
+####Make everything proportional 
+PropPlotDF = PlotDF[,c(1:11, 20:25)] %>%
+  mutate_at(vars(LOF_CountAlleles: PutNeu_CountVariants), list(~./PlotDF$CallableSites)) 
+
+####Plot Scaled Count Alleles
+scaleCalls = mean(PlotDF$CallableSites)
+ScaledPlotDF = PropPlotDF %>%
+  mutate_at(vars(LOF_CountAlleles: PutNeu_CountVariants), list(~.*scaleCalls))
+
+pSY = plotFxn(ScaledPlotDF$SY_CountAlleles, "Count Synonymous Alleles") 
+pNS = plotFxn(ScaledPlotDF$NS_CountAlleles, "Count Nonsynonymous Alleles") 
+pLOF = plotFxn(ScaledPlotDF$LOF_CountAlleles, "Count Loss of function Alleles") 
+
+# arrange the three scaled plots in a single row
+ggarrange( pSY,
            pNS + theme(axis.title.y=element_blank()),
            pLOF + theme(axis.title.y=element_blank()),
-           align = 'vh',
+           align = 'hv',
            labels = c("A", "B", "C"),
            hjust = -1,
            nrow = 1)
